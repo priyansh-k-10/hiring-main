94f46e67f2e34b7eaa585f586350ba5c4c6c2a94
unknown <pk@4fo.de>
2025-05-29 18:00:36 +0530

    making scss working

--- custom_job/__manifest__.py
+++ custom_job/__manifest__.py
@@ -23,7 +23,7 @@ Features:
     ],
      'assets': {
         'web.assets_frontend': [
-            'custom_job_page/static/src/css/job_template.css',
+            'custom_job/static/src/css/job_template.css',
         ],
     },
     'installable': True,
